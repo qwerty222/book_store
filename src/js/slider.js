c28580e66a514d4fb10fcb97b0b75d35f75104d3
qwerty222 <vladimirdrozdov88@gmail.com>
2023-10-16 19:27:31 +0400

    modify slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,5 +1,3 @@
-import styles from '../scss/slider.module.scss';
-
 import banner1 from '../img/banner_1.png';
 import banner2 from '../img/banner_2.png';
 import banner3 from '../img/banner_3.png';
@@ -7,41 +5,37 @@ import banner3 from '../img/banner_3.png';
 const banners = [banner1, banner2, banner3];
 
 function slider() {
-  const img = document.querySelector('.main__slider-image');
+  const img = document.querySelector('.slider-image');
   const dots = Array.from(document.querySelectorAll('.slider-dot'));
 
   let currentItem = 0;
+  const dotActive = 'dot-active';
 
   function changeDot(index) {
-    const activeDotClass = `.${styles.dotActive}`;
-    const activeDot = document.querySelector(activeDotClass);
-    activeDot.classList.toggle(activeDotClass);
-    dots[index].classList.toggle(activeDotClass);
-  }
+    const activeDot = document.querySelector(`.${dotActive}`);
+    activeDot.classList.toggle(dotActive);
+    dots[index].classList.toggle(dotActive);
 
-  const changePicture = (index) => {
     img.src = banners[index];
-
-    dots.forEach((dot) => {
-      if (dots.indexOf(dot) === index) {
-        dot.classList.add(styles.dotActive);
-      } else {
-        dot.classList.remove(styles.dotActive);
-      }
-    });
-  };
+  }
 
   dots.forEach((dot) => {
     dot.addEventListener('click', () => {
-      const num = dots.indexOf(dot);
-      currentItem = num;
-      changePicture(num);
+      const index = dots.indexOf(dot);
+      currentItem = index;
+      changeDot(index);
     });
   });
 
   window.addEventListener('DOMContentLoaded', () => {
-    changePicture(currentItem);
-    setInterval(changePicture, 5000);
+    changeDot(currentItem);
+    setInterval(() => {
+      currentItem += 1;
+      if (currentItem > 2) {
+        currentItem = 0;
+      }
+      changeDot(currentItem);
+    }, 5000);
   });
 }
 
