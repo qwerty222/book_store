d2573fd83029c38e94c899b57809f20a616e4051
qwerty222 <vladimirdrozdov88@gmail.com>
2023-10-24 18:41:37 +0400

    add google books api request

--- src/js/books.js
+++ src/js/books.js
@@ -1,26 +1,73 @@
 import styles from '../scss/book.module.scss';
 
-function books() {
-  const booksContainer = document.querySelector('.books-container');
-  const content = `123 <br>
-                  123 <br>
-                  123 <br>
-                  123 <br>
-                  123 <br>
-                  123 <br>
-                  123 <br>
-                  123 <br>
-                  123 <br>
-                  123 <br>
-                  123 <br>
-                  123 <br>
-                  123 <br>
-                  123 <br>
-                  123 <br>`;
+const API_KEY = 'AIzaSyCeP9nI2eoIk_3I9uSLg8TsJ7ZfHRl9U6k';
+const MAX_RESULTS = 6;
+const PRINT_TYPE = 'books';
+const LANG = 'en';
 
-  const testContent = document.createElement('p');
-  testContent.innerText = content;
-  booksContainer.appendChild(testContent);
+const prepareUrl = (startPosition, category) => {
+  const params = new URLSearchParams({
+    q: `subject:${category}`,
+    key: API_KEY,
+    printType: PRINT_TYPE,
+    startIndex: startPosition,
+    maxResults: MAX_RESULTS,
+    langRestrict: LANG,
+  });
+
+  return `https://www.googleapis.com/books/v1/volumes?${params}`;
+};
+
+const booksContainer = document.querySelector('.books-container');
+
+const showBooks = (booksData) => {
+  booksData.items.forEach((book) => {
+    const testContent = document.createElement('p');
+    testContent.innerText = book.id;
+    booksContainer.appendChild(testContent);
+  });
+};
+
+const loadData = async (category, position = 0) => {
+  try {
+    const url = prepareUrl(position, category);
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Что-то пошло не так! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
+
+    showBooks(data);
+
+    return data;
+  } catch (error) {
+    console.error('Error: ', error);
+  } finally {
+    // do something
+  }
+};
+
+function books() {
+  document.addEventListener('DOMContentLoaded', () => {
+    const d = loadData('Business');
+    // showBooks(d);
+  });
+
+  document.querySelectorAll('.category').forEach((category) => {
+    category.addEventListener('click', () => {
+      booksContainer.innerHTML = '';
+      const d = loadData(category.dataset.bookType);
+
+      const activeCategory = document.querySelector('.active');
+      activeCategory.classList.toggle('active');
+      category.classList.toggle('active');
+
+      // showBooks(d);
+    });
+  });
 }
 
 export default books;
